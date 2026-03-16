282aaa7ac49b56d27f5d26c374f4bc446305809c
Ubuntu <ubuntu@ip-172-31-43-91.ap-south-1.compute.internal>
2026-03-16 09:43:45 +0000

    Use environment variable for API URL

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -12,6 +12,8 @@ function App() {
   
   const API_URL = "https://api.ahmadansari.site/api/tasks";
 
+const API_URL = import.meta.env.VITE_API_URL;
+
   // Fetch Tasks
   useEffect(() => {
     const fetchTasks = async () => {
