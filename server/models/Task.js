0d3ac0c18016ec6288ccb458a808f57b4df8c002
Ahmadansari1942 <sn0259152@gmail.com>
2026-03-16 12:38:02 +0500

    Create Task.js

---
+++ server/models/Task.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+
+const TaskSchema = new mongoose.Schema({
+    text: {
+        type: String,
+        required: true
+    },
+    status: {
+        type: String,
+        enum: ['pending', 'completed'],
+        default: 'pending'
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+module.exports = mongoose.model('Task', TaskSchema);
